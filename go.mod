b25c6363791610f6cfee34cddd38ee5190812f6f
Patrick Furtak <patrick@patrickfurtak.com>
2020-11-07 13:22:46 -0500

    Brought in discordgo dependency, Opened up connection with discord api

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/pfurtak/gobot-gg
 
 go 1.14
+
+require github.com/bwmarrin/discordgo v0.22.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/bwmarrin/discordgo"
+)
 
 func main() {
-	fmt.Println("hello world")
+	// New bot instance
+	dg, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	// Grab bot user info
+	u, err := dg.User("@me")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	// BotID from user info
+	BotID := u.ID
+
+	// Open connection with discord
+	err = dg.Open()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	// Print Bot startup status and open a channel to keep bot running
+	fmt.Println("Bot " + BotID + "is running")
+	<-make(chan struct{})
+	return
 }
